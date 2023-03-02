9c25bed98fe1b968bdb14884702b339fbef7d455
kei <lc.kei06@gmail.com>
2023-03-02 17:35:57 +0800

    改用官方template包方便模版中引用更多变量

--- src/wechatbot.go
+++ src/wechatbot.go
@@ -6,42 +6,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"text/template"
-	"time"
 
 	"github.com/golang/glog"
+	alertmanager_template "github.com/prometheus/alertmanager/template"
 )
-
-type KV map[string]string
-type Data struct {
-	Receiver string `json:"receiver"`
-	Status   string `json:"status"`
-	Alerts   Alerts `json:"alerts"`
-
-	GroupLabels       KV `json:"groupLabels"`
-	CommonLabels      KV `json:"commonLabels"`
-	CommonAnnotations KV `json:"commonAnnotations"`
-
-	ExternalURL string `json:"externalURL"`
-}
-
-// Alert holds one alert for notification templates.
-type Alert struct {
-	Status       string    `json:"status"`
-	Labels       KV        `json:"labels"`
-	Annotations  KV        `json:"annotations"`
-	StartsAt     time.Time `json:"startsAt"`
-	EndsAt       time.Time `json:"endsAt"`
-	GeneratorURL string    `json:"generatorURL"`
-	Fingerprint  string    `json:"fingerprint"`
-}
-
-// Alerts is a list of Alert objects.
-type Alerts []Alert
 
 func main() {
 	http.HandleFunc("/wechatbot", func(w http.ResponseWriter, req *http.Request) {
 		decoder := json.NewDecoder(req.Body)
-		var t Data
+		var t alertmanager_template.Data
 
 		err := decoder.Decode(&t)
 		if err != nil {
